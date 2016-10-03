93b1db45ca1fe9f3e8807002f4e35fbad8cc2e73
root <rbalajis25@gmail.com>
2016-10-03 22:18:11 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var util = require('util');
+var http = require('http');
+var Bot  = require('@kikinteractive/kik');
+
+// Configure the bot API endpoint, details for your bot
+var bot = new Bot({
+    username: 'loklaksusi',
+    apiKey: 'ea6456c7-48aa-4718-af8d-0100b3e3422e',
+    baseUrl: 'https://loklaksusii.herokuapp.com'
+});
+
+bot.updateBotConfiguration();
+
+bot.onTextMessage((message) => {
+    message.reply(message.body);
+});
+
+// Set up your server and start listening
+http.createServer(bot.incomming()).listen(process.env.PORT)
