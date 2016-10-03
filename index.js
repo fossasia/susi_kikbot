cf50807853659027f927d78c355322e0140782dc
root <rbalajis25@gmail.com>
2016-10-04 00:08:11 +0530

    environment variabe added

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ var Bot  = require('@kikinteractive/kik');
 var susi= require('./susi.js')
 // Configure the bot API endpoint, details for your bot
 var bot = new Bot({
-    username: 'loklaksusi',
-    apiKey: 'ea6456c7-48aa-4718-af8d-0100b3e3422e',
-    baseUrl: 'https://loklaksusii.herokuapp.com'
+    username: process.env.USER_NAME,
+    apiKey: process.env.API_KEY,
+    baseUrl: process.env.HEROKU_URL
 });
 
 bot.updateBotConfiguration();
