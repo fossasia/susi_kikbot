1027bfb8c053975aade82f382434d3b688f879cf
Divyanshu N Singh <123divyanshu.n.s4@gmail.com>
2018-05-08 00:36:34 +0530

    Update SUSI API and remove unnecessary console log

--- tests/sample.js
+++ tests/sample.js
@@ -5,9 +5,4 @@ var url = "http://api.asksusi.com/susi/chat.json?q=Hi I am Shiven"
 request({
     url: url,
     json: true
-}, function (error, response, body) {
-
-    if (!error && response.statusCode === 200) {
-        console.log(body.answers[0].actions[0].expression); // Print the json response
-    }
 })
